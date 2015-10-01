f240c506f8631bdc4c0a21197cc07133fa550951
Scot Swan <mswan@sandia.gov>
2015-10-01 11:12:12 +0200

    Initial import of package plus adding documentation.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # tabfileio
+
 Tabular file input/output in multiple formats
+
+This package is used to read and write numerical tabular data. It is made for handling data in columns with a header (a text identifier). Supported formats are text, zipped text, excel (xls and xlsx), python pickle, and json.
+
+Different packages are required for different file types with output data always in a numpy.array(). The format is determined by the file extension. If the extension is unrecognized it defaults to a text format. All file formats only use packages available in the python standard library except:
+
+* .xls: *xlrd* for reading, and *xlwt* for writing
+* .xlsx: *openpyxl*
+
+The package expects column-based float data with text headers. The package was initially developed to store state variables for a simple simulation program. Here is an example of a text file demonstrating the general format:
+
+   header1  header2  header3
+       1.0      0.1      1.1
+       2.0      0.2      1.2
+       3.0      0.3      1.3
+       4.0      0.4      1.4
+
+After being processed by tabfileio.read_file() the data is represented as
+
+(['header1', 'header2', 'header3'],array([[ 1. ,  0.1,  1.1],
+                                          [ 2. ,  0.2,  1.2],
+                                          [ 3. ,  0.3,  1.3],
+                                          [ 4. ,  0.4,  1.4]]))
+
+## Testing
+
+To test the installation, run:
+
+$ python -m tabfileio
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(name='tabfileio',
+      version='0.1.0',
+      description='Tabular file input/output in multiple formats',
+      long_description=('This package is used to read and write numerical'
+                        ' tabular data. It is made for handling data in'
+                        ' columns with a header (a text identifier). Supported'
+                        ' formats are text, zipped text, excel (xls and xlsx),'
+                        ' python pickle, and json.'),
+      classifiers=[  # Classifier list:  https://pypi.python.org/pypi?:action=list_classifiers
+                   "Development Status :: 3 - Alpha",
+                   "Environment :: Console",
+                   "Intended Audience :: Developers",
+                   "Intended Audience :: Science/Research",
+                   "License :: OSI Approved :: MIT License",
+                   "Natural Language :: English",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python :: 2.7",
+                   "Programming Language :: Python :: 3",
+                   "Topic :: Database",
+                   "Topic :: Information Analysis",
+                   "Topic :: Scientific/Engineering",
+                   "Topic :: Utilities",
+                  ],
+      url='https://github.com/sswan/tabfileio',
+      author='Scot Swan',
+      author_email='scot.swan@gmail.com',
+      license='MIT',
+      packages=['tabfileio',],
+      install_requires=['numpy', 'xlrd', 'xlwt', 'openpyxl'],
+      zip_safe=False)
